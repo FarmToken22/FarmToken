8698ca825956db69cd749bae28ec869418cb19a6
FarmToken22 <mdrohimrire@gmail.com>
2025-11-05 11:20:07 +0600

    Update icon.js

--- icon.js
+++ icon.js
@@ -1,111 +1,74 @@
-/**
- * @fileoverview Sidebar Navigation Icons Configuration (Filtered + History Added)
- * @author WebGamer Studio
- * @version 1.0.2
- * @description Sidebar now includes Settings, Roadmap, Connect, and History icons.
- * @license MIT
- */
+// icon.js - Menu Icons and Links
 
-/**
- * @typedef {Object} SidebarIcon
- * @property {string} name - Display name of the navigation item
- * @property {string} href - Target URL or page reference
- * @property {string} svg - SVG markup for the icon
- * @property {string} [description] - Optional description for accessibility
- */
-
-/**
- * Sidebar navigation icons (updated)
- * @type {SidebarIcon[]}
- * @constant
- */
 export const sidebarIcons = [
-  {
-    name: 'Settings',
-    href: 'Settings.html',
-    description: 'Configure application settings',
-    svg: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-      viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" aria-hidden="true">
-      <path stroke-linecap="round" stroke-linejoin="round"
-        d="M10.325 4.317c.426-1.756 2.924-1.756 
-        3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 
-        3.31.826 2.37 2.37a1.724 1.724 0 001.065 
-        2.572c1.756.426 1.756 2.924 0 3.35a1.724 
-        1.724 0 00-1.066 2.573c.94 1.543-.826 
-        3.31-2.37 2.37a1.724 1.724 0 00-2.572 
-        1.065c-.426 1.756-2.924 1.756-3.35 
-        0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31
-        -.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c
-        -1.756-.426-1.756-2.924 0-3.35a1.724 
-        1.724 0 001.066-2.573c-.94-1.543.826
-        -3.31 2.37-2.37.996.608 2.296.096 
-        2.572-1.065z" />
-      <path stroke-linecap="round" stroke-linejoin="round"
-        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-    </svg>`,
-  },
-  {
-    name: 'Connect',
-    href: 'Connect.html',
-    description: 'Connect with other users',
-    svg: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-      viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" aria-hidden="true">
-      <path stroke-linecap="round" stroke-linejoin="round"
-        d="M8.684 13.342C8.886 12.938 9 12.482 9 12s
-        -.114-.938-.316-1.342m0 2.684a3 3 0 
-        110-2.684m0 2.684l6.632 3.316m-6.632
-        -6l6.632-3.316m0 0a3 3 0 105.367
-        -2.684 3 3 0 00-5.367 2.684zm0 
-        9.368a3 3 0 105.367 2.684 3 3 
-        0 00-5.367-2.684z" />
-    </svg>`,
-  },
-  {
-    name: 'Roadmap',
-    href: 'Roadmap.html',
-    description: 'View project roadmap',
-    svg: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-      viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" aria-hidden="true">
-      <path stroke-linecap="round" stroke-linejoin="round"
-        d="M9 20l-5.447-2.724A1 1 0 
-        013 16.382V5.618a1 1 0 011.447
-        -.894L9 7m0 13l6-3m-6 3V7m6 
-        10l5.447 2.724A1 1 0 0021 
-        16.382V5.618a1 1 0 00-1.447
-        -.894L15 7" />
-    </svg>`,
-  },
-  {
-    name: 'History',
-    href: 'History.html',
-    description: 'View transaction or activity history',
-    svg: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-      viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-      <path stroke-linecap="round" stroke-linejoin="round"
-        d="M12 8v4l3 3m6-3a9 9 0 
-        11-9-9 9 9 0 019 9zM4.5 12A7.5 
-        7.5 0 1012 4.5" />
-    </svg>`,
-  },
+    {
+        name: 'Home',
+        href: 'index.html',
+        svg: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+        </svg>`
+    },
+    {
+        name: 'Wallet',
+        href: 'wallet.html',
+        svg: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
+        </svg>`
+    },
+    {
+        name: 'Bonus',
+        href: 'Bonus.html',
+        svg: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
+        </svg>`
+    },
+    {
+        name: 'Tasks',
+        href: 'tasks.html',
+        svg: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
+        </svg>`
+    },
+    {
+        name: 'Leaderboard',
+        href: 'leaderboard.html',
+        svg: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+        </svg>`
+    },
+    {
+        name: 'Settings',
+        href: 'settings.html',
+        svg: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+        </svg>`
+    },
+    {
+        name: 'Support',
+        href: 'support.html',
+        svg: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>`
+    },
+    {
+        name: 'About',
+        href: 'about.html',
+        svg: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>`
+    },
+    {
+        name: 'Referrals',
+        href: '#referrals',
+        svg: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+        </svg>`
+    },
+    {
+        name: 'Transactions',
+        href: 'transactions.html',
+        svg: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
+        </svg>`
+    }
 ];
-
-/** Utility functions **/
-export const getSidebarIconByName = (name) =>
-  sidebarIcons.find((icon) => icon.name === name);
-
-export const getSidebarIconByHref = (href) =>
-  sidebarIcons.find((icon) => icon.href === href);
-
-export const getAllIconNames = () =>
-  sidebarIcons.map((icon) => icon.name);
-
-export const isValidIcon = (icon) =>
-  icon &&
-  typeof icon.name === 'string' &&
-  typeof icon.href === 'string' &&
-  typeof icon.svg === 'string' &&
-  icon.name.length > 0 &&
-  icon.href.length > 0 &&
-  icon.svg.length > 0;
-
-export default sidebarIcons;
